8214e62c946451c2e45e82b70e636b1ef7b222b9
gil <gilovi@users.noreply.github.com>
2016-01-12 13:33:59 +0200

    constructors added and rearranged methods

--- Matrix.hpp
+++ Matrix.hpp
@@ -8,7 +8,10 @@
 #include <vector>
 #include <iterator>
 #include "Complex.h"
+#include <stdexcept>
 
+#define DEFAULT_SIZE 1
+#define OUT_OF_RANGE_MSG "index is outside the matrix's range"
 
 /**
 * Generic matrix class.
@@ -18,52 +21,49 @@ class Matrix
 {
 public:
     typedef typename std::vector<T>::const_iterator const_iterator;
+
+    /**
+    * Dimension constructor.
+    * @param rows the number of rows
+    * @param cols the number of columns
+    */
+    Matrix<T>(unsigned int rows, unsigned int cols) : _rows(rows), _cols(cols),  _matrix(rows*cols ,T(0))
+    {}
+
     /**
     * Default constructor.
     */
-    Matrix<T>() : _rows(1), _cols(1)
-    {
+    Matrix<T>() : Matrix(DEFAULT_SIZE, DEFAULT_SIZE)
+    {}
 
-    }
-
-    /**
-    * Dimensional constructor.
-    * @param rows number of rows
-    * @param cols number of columns
-    */
-    Matrix<T>(unsigned int rows, unsigned int cols) : _rows(rows), _cols(cols)
-    {
-
-    }
 
     /**
     * Copy constructor.
-    * @param matrixToCopy source matrix
+    * @param toCopy matrix to copy
     */
-    Matrix<T>(const Matrix<T>& matrixToCopy)
+    Matrix<T>(const Matrix<T>& toCopy)
     {
-        *this = matrixToCopy;
+        *this = toCopy;
     }
 
     /**
     * Move constructor.
-    * @param matrixToMove source matrix
+    * @param toMove the matrix to move
     */
-    Matrix<T>(const Matrix<T> && matrixToMove) :
-            _matrix(std::move(matrixToMove._matrix)),
-            _rows(std::move(matrixToMove._rows)),
-            _cols(std::move(matrixToMove._cols)){}
+    Matrix<T>(const Matrix<T>&& toMove) :
+            _rows(std::move(toMove._rows)),
+            _cols(std::move(toMove._cols)),
+            _matrix(std::move(toMove._matrix))
+    {}
 
     /**
-    * Content constructor.
+    * Values constructor.
     * @param rows number of rows in matrix
     * @param cols number of columns in matrix
-    * @param cells set of values to instert into matrix
+    * @param cells values to insert into the matrix
     */
-    Matrix<T>(unsigned int rows, unsigned int cols, const std::vector<T>& cells) : _rows(rows), _cols(cols)
-    {
-
-    }
+    Matrix<T>(unsigned int rows, unsigned int cols, const std::vector<T>& cells) : _rows(rows), _cols(cols), _matrix(cells.begin(), cells.end())
+    {}
 
     /**
     * Destructor.
@@ -74,54 +74,15 @@ public:
     }
 
     /**
-    * Setter () operator.
-    * @param row row index
-    * @param col column index
-    * @return the relevant value in matrix
-    * @throw OutOfMatrixLimitsException if input indexes are out of matrix size
-    */
-    T& operator()(unsigned int row, unsigned int col)
-    {
-
-    }
-
-    /**
-    * Getter () operator.
-    * @param row row index
-    * @param col column index
-    * @return the relevant value in matrix
-    * @throw OutOfMatrixLimitsException if input indexes are out of matrix size
-    */
-    const T& operator()(unsigned int row, unsigned int col) const
-    {
-
-    }
-
-    /**
-    * Equality operator.
-    * @param rhs second matrix in the operation
-    */
-    bool operator==(const Matrix<T>& rhs) const
-    {
-
-    }
-
-    /**
-    * Non-Equality operator.
-    * @param rhs second matrix in the operation
-    */
-    bool operator!=(const Matrix<T>& rhs) const
-    {
-        return not(*this == rhs);
-    }
-
-    /**
     * Assignment operator.
-    * @param rhs matrix to assign
+    * @param rhs the matrix to assign
     */
     Matrix<T>& operator=(Matrix<T> rhs)
     {
-
+        std::swap(this->_rows, rhs._rows);
+        std::swap(this->_cols, rhs._cols);
+        std::swap(this->_matrix, rhs._matrix);
+        return *this;
     }
 
     /**
@@ -155,6 +116,29 @@ public:
     }
 
     /**
+    * Equality operator.
+    * @param rhs second matrix in the operation
+    */
+    bool operator==(const Matrix<T>& rhs) const
+    {
+        if (this->_rows == rhs._rows && this->_cols == rhs._cols)
+        {
+            //TODO!!
+        }
+
+        return false;
+    }
+
+    /**
+    * Non-Equality operator.
+    * @param rhs second matrix in the operation
+    */
+    bool operator!=(const Matrix<T>& rhs) const
+    {
+        return not(*this == rhs);
+    }
+
+    /**
     * Matrix transpose creator.
     * @return transposed matrix
     */
@@ -169,6 +153,44 @@ public:
     T trace() const
     {
 
+    }
+
+    /**
+    * Matrix printing operator.
+    * @param matrix right-side matrix
+    * @return output stream
+    */
+    friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& matrix)
+    {
+
+    }
+
+    /**
+    * Index () operator.
+    * @param row the row index
+    * @param col the column index
+    * @return the indexed value in the matrix
+    * @throw out_of_range if input indexes are out of matrix size
+    */
+    T& operator()(unsigned int row, unsigned int col)
+    {
+        if (row > this->_rows || col > this->_cols)
+        {
+            throw std::out_of_range (OUT_OF_RANGE_MSG);
+        }
+        return this->_matrix[getIndex(row, col)];
+    }
+
+    /**
+    * Const index () operator.
+    * @param row the row index
+    * @param col the column index
+    * @return the indexed value in the matrix
+    * @throw OutOfMatrixLimitsException if input indexes are out of matrix size
+    */
+    const T& operator()(unsigned int row, unsigned int col) const
+    {
+        return this->_matrix[getIndex(row, col)];
     }
 
     /**
@@ -189,7 +211,7 @@ public:
 
     /**
     * Rows getter.
-    * @return number of rows in matrix
+    * @return the number of rows in the matrix
     */
     unsigned int rows() const
     {
@@ -198,7 +220,7 @@ public:
 
     /**
     * Columns getter.
-    * @return number of columns in matrix
+    * @return the number of columns in the matrix
     */
     unsigned int cols() const
     {
@@ -224,10 +246,15 @@ public:
     }
 
 private:
-    std::vector<T> _matrix;
     unsigned int _rows;
     unsigned int _cols;
+    std::vector<T> _matrix;
 
+
+    int getIndex(unsigned int row,  unsigned int col) const
+    {
+        return (row * this->_cols) + col;
+    }
 
     /**
     * A Function that check if matrix is square or not.
@@ -243,17 +270,6 @@ private:
 
 
 };
-
-/**
-* Matrix printing operator.
-* @param matrix right-side matrix
-* @return output stream
-*/
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const Matrix<T>& matrix)
-{
-
-}
 
 /**
 * Conjugate matrix creator. Works on complex matrices only.
